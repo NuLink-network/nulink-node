b685d0671b4d5b2ea463c7e18cbe012e43f16494
relxss <relxgo@163.com>
2022-02-23 19:39:43 +0800

    add example code

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	engine := gin.Default()
+
+	engine.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	log.Panic(engine.Run())
 }
